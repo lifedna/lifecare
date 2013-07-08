c900eb1de626906a4088724a84b4ec28f3193d61
wangyang <wangyang@wangyang-Latitude-E6530.(none)>
2013-07-08 18:59:02 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifecare::Application.config.secret_token = '5a2aa710fd30f10d05adb48dcdcf6bbdac89e26a48d91c51b9b967d56c2a302c4063beb9782d5fe677d2cd6d45b6112bdd70646a81e86ffaa69b5f64791f4b63'
