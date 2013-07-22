b624bd6346c5f8b253ab1de9816d1619a3b919e5
wangyang <wangyang@wangyang-Latitude-E6530.(none)>
2013-07-22 16:45:18 +0800

    add content model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,36 +10,25 @@ class HomeController < ApplicationController
   end
 
   def update
-    # communities = current_user.communities
-    # @activities = Activity.all_update(communities).page params[:page]
-    
-    # if user_signed_in?
-    #   @activities ||= []
-    #   current_user.communities.each do |community|
-    #     # @activities.concat(community.activities_since_jion(current_user))
-    #     @activities.concat(community.activities)
-
-    #   end 
-    #   @activities.sort_by!{|i| i.created_at}.reverse! unless @activities.empty?
-    # end
   end
   
   def features
     @feature_filter = current_user.feature_filter
     @keywords = current_user.feature_filter.merged_keywords
-    @items ||= []
+
     if params[:category]
-      @blogs = Blog.where(category: params[:category]).full_text_search(@keywords)
-      @articles = Article.where(category: params[:category]).full_text_search(@keywords)
-      @topics = Topic.where(category: params[:category]).full_text_search(@keywords)
+      if @keywords.nil? or @keywords.empty?
+        @items = Content.where(category: params[:category])
+      else
+        @items = Content.where(category: params[:category]).full_text_search(@keywords)
+      end  
     else
-      @blogs = Blog.all.full_text_search(@keywords)
-      @articles = Article.all.full_text_search(@keywords)
-      @topics = Topic.all.full_text_search(@keywords)
-    end  
-    @items.concat(@blogs)
-    @items.concat(@articles)
-    @items.concat(@topics)
+      if @keywords.nil? or @keywords.empty?
+        @items = Content.all
+      else
+        @items = Content.all.full_text_search(@keywords)
+      end 
+    end 
   end
 
   def mine
